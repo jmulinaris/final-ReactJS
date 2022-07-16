f57a236d4738d47e326db72c1605f1523c12bedf
julimulinaris <juli.mulinaris@gmail.com>
2022-07-15 23:07:07 -0300

    uso base de datos de firestore

--- src/Components/ItemDetailContainer/index.jsx
+++ src/Components/ItemDetailContainer/index.jsx
@@ -2,19 +2,22 @@ import React, {useState, useEffect} from 'react'
 import ItemDetail from '../ItemDetail'
 import "./styles.css"
 import {getProduct} from "../../Mocks/fakeApi"
-import { useParams } from 'react-router-dom'
-import { BeatLoader } from "react-spinners";
+import {useParams} from 'react-router-dom'
+import {BeatLoader} from "react-spinners";
+import {db} from "../../firebase/firebase";
+import { doc, getDoc} from "firebase/firestore";
 
 const ItemDetailContainer = () => {
     const [product, setProduct] = useState({});
     const [loading, setLoading] = useState(true);
-
     const {id} = useParams();
 
     useEffect(()=>{
-        getProduct (id)
-        .then((res)=>{
-            setProduct (res);
+
+        const refDoc = doc(db, "productos", id);
+        getDoc(refDoc)
+        .then (result =>{
+            setProduct( {id:result.id, ...result.data()});
         })
         .catch((error) =>{
             console.log(error);
