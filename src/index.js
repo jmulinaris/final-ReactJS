61fbae2ac48fb6b1942ecf8ed7649faa1be7239e
julimulinaris <juli.mulinaris@gmail.com>
2022-07-29 21:31:36 -0300

    elimino archivos que no uso

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import "./firebase/firebase"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,6 +10,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-
-reportWebVitals();
